4a4eee338e32192cb343d1e68568d5971e2f9815
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2016-12-27 11:31:03 -0200

    Aula 05 - Cadastrando Post e Listando

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class HomeController extends Controller
 {
     /**
@@ -21,8 +23,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Post $post)
     {
-        return view('home');
+        $posts = $post->all();
+
+        return view('home', compact('posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="container">
+        @forelse ($posts as $post)
+            <h1>{{$post->title}}</h1>
+            <p>{{$post->description}}</p>
+            <strong>Autor:</strong> {{$post->user->name}}
+            <hr>
+        @empty
+            <p>Nenhum post cadastrado!</p>
+        @endforelse
     </div>
-</div>
 @endsection
